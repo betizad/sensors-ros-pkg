993abec65ce6db86f6a02b7ec3b814816d4c69e6
Ivor Rendulic <ivor.rendulic@gmail.com>
2015-05-07 12:28:37 +0200

    Separated Curl-based jpeg camera publisher from opencv camera publisher.

--- image_processing/camera_image_processing/src/CurlCameraDriver.cpp
+++ image_processing/camera_image_processing/src/CurlCameraDriver.cpp
@@ -39,10 +39,8 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
-#include <labust/sensors/image/OpenCvCameraDriver.hpp>
+#include <labust/sensors/image/CurlCameraDriver.hpp>
 #include <labust/sensors/image/ImageProcessingUtil.hpp>
-
-#include <opencv2/opencv.hpp>
 
 #include <signal.h>
 
@@ -55,81 +53,71 @@ void sigint_handler(int sig) {
   ros::shutdown();
 }
 
+size_t write_data(void* ptr, size_t size, size_t nmemb, sensor_msgs::CompressedImage::_data_type* stream) {
+  uint8_t* pt = reinterpret_cast<uint8_t*>(ptr);
+  stream->insert(stream->end(),pt, pt + (size*nmemb));
+  return size*nmemb;
+}
+
 
 /**
- * ROS node for publishing videos from external cameras.
+ * ROS node for publishing jpeg stream.
  */
-OpenCvCameraDriver::OpenCvCameraDriver(ros::NodeHandle camera_nh, ros::NodeHandle ph) :
+CurlCameraDriver::CurlCameraDriver(ros::NodeHandle camera_nh, ros::NodeHandle ph) :
     ph_(ph),
     camera_nh_(camera_nh),
-    it_(camera_nh_),
-    image_pub_(it_.advertiseCamera("image_raw", 1)),
     camera_info_manager_(camera_nh_),
     camera_info_url_(""),
-    is_video_(true),
-    camera_address_("device"),
-    device_id_(0) {
-  ph.getParam("is_video", is_video_);
+    curl(0),
+    camera_address_("") {
   ph.getParam("camera_address", camera_address_);
-  ph.getParam("device_id", device_id_);
   ph.getParam("camera_info_url", camera_info_url_);
   
+  compressed_image_pub_ = camera_nh_.advertise<sensor_msgs::CompressedImage>("/camera/image_raw/compressed", 1);
+  compressed_camera_info_pub_ = camera_nh_.advertise<sensor_msgs::CameraInfo>("/camera/camera_info", 1);
+      
   if (camera_info_url_ != "") {
     camera_info_manager_.loadCameraInfo(camera_info_url_);
   }
-}
-
-OpenCvCameraDriver::~OpenCvCameraDriver() {
-  if (is_video_) {
-    video_capture_.release();
-  }
-}
-
-void OpenCvCameraDriver::setup() {
-  // If target is a valid video stream, open it now.
-  if (is_video_) {
-    if (camera_address_ == "device") {
-      video_capture_.open(device_id_);
-    } else {
-      video_capture_.open(camera_address_);
-    }
-  }
-}
-
-void OpenCvCameraDriver::poll() {
-  cv::Mat frame;
   
-  // If target is a valid video stream, read one frame.
-  // If target is not a valid video stream (e.g. a stream of .jpeg images), open image and read it.
-  bool successful_read;
-  if (is_video_) {
-    successful_read = video_capture_.read(frame);
-  } else {
-    video_capture_.open(camera_address_);
-    successful_read = video_capture_.read(frame);
+  curl = curl_easy_init();
+  if (curl == 0) {
+    ROS_ERROR("Failed to init Curl.");
   }
-  if (!successful_read) {
-    ROS_WARN("No frame");
+  curl_easy_setopt(curl, CURLOPT_URL, camera_address_.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+}
+
+CurlCameraDriver::~CurlCameraDriver() {
+  if (curl) {
+    curl_easy_cleanup(curl);
   }
-  // Convert cv::Mat image to sensor_msgs::Image and publish it.
-  sensor_msgs::CameraInfoPtr ci(new sensor_msgs::CameraInfo(camera_info_manager_.getCameraInfo()));
-  sensor_msgs::ImagePtr sensor_image = cvImage2SensorImage(frame, "bgr8");
-  image_pub_.publish(sensor_image, ci);
-  if (!is_video_) video_capture_.release();
+}
+
+
+void CurlCameraDriver::poll() {
+  sensor_msgs::CompressedImagePtr compressed_image(new sensor_msgs::CompressedImage());
+  compressed_image->format = "bgr8";
+  compressed_image->format += "; jpeg compressed";
+  
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &compressed_image->data);
+  CURLcode res = curl_easy_perform(curl);
+
+  compressed_image_pub_.publish(compressed_image);
+  compressed_camera_info_pub_.publish(camera_info_manager_.getCameraInfo());
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "opencv_camera_driver_node");
+  ros::init(argc, argv, "curl_camera_driver_node");
   ros::NodeHandle camera_nh("camera"), ph("~");
   
-  OpenCvCameraDriver ex_cam(camera_nh, ph);
+  CurlCameraDriver curl_cam(camera_nh, ph);
   signal(SIGINT, &sigint_handler);
-  ex_cam.setup();
   
   ros::Rate r(25);
   while (ros::ok()) {
     r.sleep();
-    ex_cam.poll();
+    curl_cam.poll();
     ros::spinOnce();
   }
 
