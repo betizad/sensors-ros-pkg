9b35a1c4b58032fbc05e46fbcb30d0fff9e47440
PlaDyPos <labust-unizg@gmail.com>
2015-06-23 07:13:28 +0200

    Merge.

--- novatel_dgps/scripts/get_share.sh
+++ novatel_dgps/scripts/get_share.sh
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
 #!/bin/bash
 socat -d -d -u tcp:$2,nodelay,forever $1,raw,echo=0,b115200 
 exit 0
+=======
+socat -d -d -u tcp:$2,nodelay $1,raw,echo=0,b115200 
+>>>>>>> Stashed changes
